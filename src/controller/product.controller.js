9229cc843c3e8fd33a4c3596a5e0040d571c0599
DuongKhanhAn <119850695+DuongKhanhAn@users.noreply.github.com>
2026-01-09 14:55:58 +0700

    Section 16: Add tính năng isDraft, isPublish, unPublish và bổ sung schema Product Service API

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -2,6 +2,7 @@
 
 const ProductService = require("../services/product.service")
 const  { SuccessResponse } = require('../core/success.response')
+const { product } = require("../models/product.model")
 
 class ProductController{
     
@@ -14,6 +15,59 @@ class ProductController{
             })
         }).send(res)
     }
+
+    publishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'publishProductByShop success!',
+            metadata: await ProductService.publishProductByShop({
+                product_id: req.params.id,
+                product_shop: req.user.userId
+            })
+        }).send(res)       
+    }
+
+    unPublishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'unPublishProductByShop success!',
+            metadata: await ProductService.unPublishProductByShop({
+                product_id: req.params.id,
+                product_shop: req.user.userId
+            })
+        }).send(res)       
+    }
+    
+    // QUERY //
+    /**
+     * @desc Get all Drafts for shop
+     * @param {Number} limit
+     * @param {Number} skip
+     * @return { JSON }
+     */
+    getAllDraftsForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list Draft success!',
+            metadata: await ProductService.findAllDraftsForShop({
+                product_shop: req.user.userId
+            })
+        }).send(res)  
+    }
+
+    getAllPublishForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list Publish success!',
+            metadata: await ProductService.findAllPublishForShop({
+                product_shop: req.user.userId
+            })
+        }).send(res)  
+    }
+
+    getListSearchProduct = async (req, res, next) => {
+        new SuccessResponse({
+            message: 'Get list Search Product success!',
+            metadata: await ProductService.searchProducts(req.params)
+        }).send(res)  
+    }
+    // END QUERY //
 }
 
 module.exports = new ProductController()
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,13 +9,7 @@ router.use(apiKey)
 // check permission
 router.use(permission('0000'))
 
-router.use('/v1/api', require('./access'))
-// router.get('/', (req, res, next) => {
-//     return res.status(200).json({
-//         message: 'Welcome!',
-//     });
-// })
-
 router.use('/v1/api/product', require('./product'))
+router.use('/v1/api', require('./access'))
 
 module.exports = router
